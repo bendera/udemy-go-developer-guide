3bf3e8cbc1c7722d5bb0488ee02774bc07698c9e
Adam Bender <szoftverhiba@gmail.com>
2019-01-03 21:02:32 +0100

    20. Creating a new deck #2

--- deck.go
+++ deck.go
@@ -5,7 +5,16 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
-	cards := deck{"Ace of Spades", "Two of Spades"}
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
 
 	return cards
 }
