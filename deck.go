ccca9cf7c993649ab440534b43a1fa8de130e2bc
Adam Bender <szoftverhiba@gmail.com>
2019-01-04 00:10:14 +0100

    29. Shuffling a deck

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -53,4 +54,11 @@ func newDeckFromFile(filename string) deck {
 	s := strings.Split(string(bs), ",")
 
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	cards := newDeckFromFile("cards.txt")
+	cards.shuffle()
 	cards.print()
 }
