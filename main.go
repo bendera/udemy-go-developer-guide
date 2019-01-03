cfd8f3f6b52ac99b96bcb185289182d0e22cf8f0
abender <bender.adam@netpositive.hu>
2019-01-03 18:27:40 +0100

    18. Custom type declarations

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
