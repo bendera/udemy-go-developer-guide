92dfaedd8078f29d091324781d33a03f9c81ff0a
abender <bender.adam@netpositive.hu>
2019-01-03 18:55:08 +0100

    20. Creating a new deck

--- deck.go
+++ deck.go
@@ -4,6 +4,12 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{"Ace of Spades", "Two of Spades"}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
