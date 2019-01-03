6ae65727ae7b2e9d7cebf15a2bc58cfac2c1b4c5
abender <bender.adam@netpositive.hu>
2019-01-03 18:17:47 +0100

    Slices and for loops #3

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ func main() {
 	cards := []string{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
