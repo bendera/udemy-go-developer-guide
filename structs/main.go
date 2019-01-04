c50fc59caeb9731628b8409a4d10f2d80d1fda5f
abender <bender.adam@netpositive.hu>
2019-01-04 15:21:45 +0100

    38. Declaring structs

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	alex := person{"Alex", "Anderson"}
 
+	fmt.Println(alex.firstName, alex.lastName)
 }
