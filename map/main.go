fd624bc509b3d3e3618a4e10d33f819c44cac595
Adam Bender <szoftverhiba@gmail.com>
2019-01-06 00:44:36 +0100

    49. Manipulating maps

--- map/main.go
+++ map/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	/* colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
 		"blue":  "#0000ff",
-	}
+	} */
+
+	var colors map[string]string
 
 	fmt.Println(colors)
 }
