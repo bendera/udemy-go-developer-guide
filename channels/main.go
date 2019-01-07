7ea7e6dd3691ffe82bd9eefcc060d87903ad1e9a
Adam Bender <szoftverhiba@gmail.com>
2019-01-08 00:58:52 +0100

    77. Repeating routines

--- channels/main.go
+++ channels/main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -30,11 +30,11 @@ func checkLink(link string, c chan string) {
 
 	if err != nil {
 		fmt.Println(link, "link might be down")
-		c <- "Might be down"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Is up"
+	c <- link
 	return
 }
